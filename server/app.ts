ee239a1d0a96cfbdce0ee88de6968106cccb7687
Muhammad Muaaz <MuhammadMuaaz007@users.noreply.github.com>
2026-04-21 13:06:50 +0500

    Project Set up

---
+++ server/app.ts
@@ -0,0 +1,34 @@
+import dotenv from "dotenv";
+import express, { NextFunction, Request, Response } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+dotenv.config();
+export const app = express();
+
+// body parser
+app.use(express.json({ limit: "50mb" }));
+
+// cookie parser
+app.use(cookieParser());
+
+// cors => cross origin resource sharing
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+  })
+);
+
+// testing api
+app.get("/test", (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).json({
+    success: true,
+    message: "API is working",
+  });
+});
+// unknown route
+app.all(/.*/,(req: Request, res: Response, next: NextFunction) => {
+  const err = new Error(`Route ${req.originalUrl} not found`) as any;
+  err.statusCode = 404;
+  next(err);
+});
