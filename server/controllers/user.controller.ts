a4846a16ec4d0b040ec3e8e53cd0ec1e973c5e35
Muhammad Muaaz <MuhammadMuaaz007@users.noreply.github.com>
2026-04-23 11:44:11 +0500

    Add logout Routes

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -143,3 +143,18 @@ export const loginUser = CatchAsyncError(async (req: Request, res: Response, nex
   }
 });
 
+// logout user
+
+export const logoutUser=CatchAsyncError(async(req:Request,res:Response,next:NextFunction)=>{
+  try {
+    res.cookie("access_token","",{maxAge:1});
+    res.cookie("refresh_token","",{maxAge:1});
+    res.status(200).json({
+      success:true,
+      message:"Logged out successfully"
+    })
+  } catch (error:any) {
+    return next(new ErrorHandler(error.message,400))
+  }
+})
+
--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -1,9 +1,10 @@
 import express from "express";
-import { activateUser, loginUser, registrationUser } from "../controllers/user.controller.js";
+import { activateUser, loginUser, logoutUser, registrationUser } from "../controllers/user.controller.js";
 const userRouter = express.Router();
 
 userRouter.post("/registration", registrationUser);
 userRouter.post("/activate-user", activateUser);
 userRouter.post("/login",loginUser);
+userRouter.get("/logout",logoutUser);
 
 export default userRouter;
