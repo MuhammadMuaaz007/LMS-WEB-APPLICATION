a0ab4786d109cd3ddbfe0a164a6a9d38d5c2bb08
Muhammad Muaaz <MuhammadMuaaz007@users.noreply.github.com>
2026-04-24 12:16:10 +0500

    Enhance JWT token signing methods with expiration options and update user route for token refresh

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { activateUser, loginUser, logoutUser, registrationUser } from "../controllers/user.controller.js";
+import { activateUser, loginUser, logoutUser, registrationUser, updateAccessToken } from "../controllers/user.controller.js";
 import { isAuthenticated } from "../middleware/auth.js";
 const userRouter = express.Router();
 
@@ -7,5 +7,6 @@ userRouter.post("/registration", registrationUser);
 userRouter.post("/activate-user", activateUser);
 userRouter.post("/login",loginUser);
 userRouter.get("/logout",isAuthenticated,logoutUser);
+userRouter.get("/refresh",updateAccessToken);
 
 export default userRouter;
--- server/utils/jwt.ts
+++ server/utils/jwt.ts
@@ -11,6 +11,25 @@ interface ITokenOptions {
   sameSite: 'lax' | 'strict' | 'none' | undefined;
   secure?: boolean;
 }
+ // parse environment variables to integrates with fallback values
+  export const accessTokenExpire = parseInt(process.env.ACCESS_TOKEN_EXPIRE || '300', 10);
+  export const refreshTokenExpire = parseInt(process.env.REFRESH_TOKEN_EXPIRE || '1200', 10);
+
+  // options for cookies
+  export const accessTokenOptions: ITokenOptions = {
+    expires: new Date(Date.now() + accessTokenExpire *60*60 * 1000),
+    maxAge: accessTokenExpire *60 *60* 1000,
+    httpOnly: true,
+    sameSite: 'lax',
+  };
+
+  export const refreshTokenOptions: ITokenOptions = {
+    expires: new Date(Date.now() + refreshTokenExpire *24* 60 * 60 * 1000),
+    maxAge: refreshTokenExpire * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    sameSite: 'lax',
+  };
+
 
 export const sendToken = (user: IUser, statusCode: number, res: Response) => {
   const accessToken = user.SignAccessToken();
@@ -19,25 +38,7 @@ export const sendToken = (user: IUser, statusCode: number, res: Response) => {
   // upload session to redis
   redis.set(user._id.toString(),JSON.stringify(user as any))
 
-  // parse environment variables to integrates with fallback values
-  const accessTokenExpire = parseInt(process.env.ACCESS_TOKEN_EXPIRE || '300', 10);
-  const refreshTokenExpire = parseInt(process.env.REFRESH_TOKEN_EXPIRE || '1200', 10);
-
-  // options for cookies
-  const accessTokenOptions: ITokenOptions = {
-    expires: new Date(Date.now() + accessTokenExpire * 1000),
-    maxAge: accessTokenExpire * 1000,
-    httpOnly: true,
-    sameSite: 'lax',
-  };
-
-  const refreshTokenOptions: ITokenOptions = {
-    expires: new Date(Date.now() + refreshTokenExpire * 1000),
-    maxAge: refreshTokenExpire * 1000,
-    httpOnly: true,
-    sameSite: 'lax',
-  };
-
+ 
   // only set secure to true in production
   if (process.env.NODE_ENV === 'production') {
     accessTokenOptions.secure = true;
@@ -46,7 +47,7 @@ export const sendToken = (user: IUser, statusCode: number, res: Response) => {
 
   res.cookie("access_token", accessToken, accessTokenOptions);
   res.cookie("refresh_token", refreshToken, refreshTokenOptions);
-  
+
   res.status(statusCode).json({
     success: true,
     user,
