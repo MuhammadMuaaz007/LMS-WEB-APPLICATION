c23340c67abc0d3f95bf0379804bef3d6b87d0d0
Muhammad Muaaz <MuhammadMuaaz007@users.noreply.github.com>
2026-04-25 12:53:24 +0500

    add Social Auth controller and the route

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -215,3 +215,31 @@ export const getUserInfo = CatchAsyncError(
   }
 );
 
+// social auth
+interface ISocialAuthBody {
+  email: string;
+  name: string;
+  avatar: {
+    public_id: string;
+    url: string;
+  };
+}
+
+export const socialAuth = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { email, name, avatar } = req.body as ISocialAuthBody;
+      const user = await userModel.findOne({ email });
+
+      if (!user) {
+        const newUser = await userModel.create({ email, name, avatar });
+        sendToken(newUser, 200, res);
+      } else {
+        sendToken(user, 200, res);
+      }
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 400));
+    }
+  }
+);
+
