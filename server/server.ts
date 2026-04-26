7592de3b5b857262551225eff1a3719308b6a33c
Muhammad Muaaz <MuhammadMuaaz007@users.noreply.github.com>
2026-04-26 12:52:04 +0500

    Refactor server.ts for consistent formatting and improved cloudinary configuration

--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,17 @@
-import {app} from './app.js';
-import dotenv from 'dotenv';
+import { app } from "./app.js";
+import dotenv from "dotenv";
 dotenv.config();
-import connectDB from './utils/db.js';
+import connectDB from "./utils/db.js";
+import cloudinary from "cloudinary";
+
+cloudinary.v2.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_SECRET_KEY,
+});
 
 //create server
 app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
-    connectDB();
+  console.log(`Server is running on port ${process.env.PORT}`);
+  connectDB();
 });
-
