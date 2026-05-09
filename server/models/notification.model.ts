39d8665b5be7ae1a947b578fca9771301ff2de65
Muhammad Muaaz <MuhammadMuaaz007@users.noreply.github.com>
2026-05-09 16:30:51 +0500

    Add Notification model with schema and interface

---
+++ server/models/notification.model.ts
@@ -0,0 +1,37 @@
+import mongoose, { Schema, Model, Document } from "mongoose";
+
+export interface INotification extends Document {
+  title: string;
+  userId: string;
+  message: string;
+  status: string;
+}
+
+const notificationSchema = new Schema<INotification>(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    userId: {
+      type: String,
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      default: "unread",
+    },
+  },
+  { timestamps: true },
+);
+
+const NotificationModel: Model<INotification> = mongoose.model(
+  "Notification",
+  notificationSchema,
+);
+export default NotificationModel;
