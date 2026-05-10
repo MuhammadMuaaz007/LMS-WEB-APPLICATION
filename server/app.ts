58ed95eea36116bc34db5c861e179e49fa87695b
Muhammad Muaaz <MuhammadMuaaz007@users.noreply.github.com>
2026-05-10 19:09:31 +0500

    Refactor app.ts to consolidate route handling for user, course, and order routers

--- server/app.ts
+++ server/app.ts
@@ -6,6 +6,7 @@ import cookieParser from "cookie-parser";
 import userRouter from "./routes/user.route.js";
 import ErrorMiddleware from "./middleware/error.js";
 import courseRouter from "./routes/course.route.js";
+import orderRouter from "./routes/order.route.js";
 
 dotenv.config();
 export const app = express();
@@ -13,24 +14,14 @@ export const app = express();
 app.use(express.json({ limit: "50mb" }));
 // cookie parser
 app.use(cookieParser());
-// cors => cross origin resource sharing
+
 app.use(
   cors({
     origin: process.env.ORIGIN,
     credentials: true,
   }),
 );
-app.use("/api/v1", userRouter);
-app.use("/api/v1", courseRouter);
-
-// testing api
-// app.get("/test", (req: Request, res: Response, next: NextFunction) => {
-//   res.status(200).json({
-//     success: true,
-//     message: "API is working",
-//   });
-// });
-// unknown route
+app.use("/api/v1", userRouter, courseRouter, orderRouter);
 
 app.all(/.*/, (req: Request, res: Response, next: NextFunction) => {
   const err = new Error(`Route ${req.originalUrl} not found`) as any;
