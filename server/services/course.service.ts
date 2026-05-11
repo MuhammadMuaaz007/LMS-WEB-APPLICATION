1d4307ffd9fc4d7a70870df1c74988230f6df4d6
Muhammad Muaaz <MuhammadMuaaz007@users.noreply.github.com>
2026-05-11 23:13:54 +0500

    Update comment for createCourse function to clarify its purpose

--- server/services/course.service.ts
+++ server/services/course.service.ts
@@ -2,7 +2,7 @@ import { Response,NextFunction } from "express";
 import CourseModel from "../models/course.model.js";
 import { CatchAsyncError } from "../middleware/catchAsyncErrors.js";
 
-// create course
+// create course service
 export const createCourse = CatchAsyncError(
   async (data: any, res: Response,next:NextFunction) => {
     const course = await CourseModel.create(data);
