ef57251874818bad78c6a2f007a8f9688b8f43f4
Muhammad Muaaz <MuhammadMuaaz007@users.noreply.github.com>
2026-05-16 22:53:29 +0500

    Refactor notification route: consolidate import statements and ensure updateNotificationStatus is correctly defined

--- server/routes/notification.route.ts
+++ server/routes/notification.route.ts
@@ -1,6 +1,9 @@
 import express from "express";
 import { authorizeRole, isAuthenticated } from "../middleware/auth.js";
-import { getNotifications } from "../controllers/notification.controller.js";
+import {
+  getNotifications,
+  updateNotificationStatus,
+} from "../controllers/notification.controller.js";
 
 const notificationRoute = express.Router();
 
@@ -9,6 +12,12 @@ notificationRoute.get(
   isAuthenticated,
   authorizeRole("admin"),
   getNotifications,
+);
+
+notificationRoute.put(
+  "/update-notification-status/:id",
+  isAuthenticated,
+  updateNotificationStatus,
 );
 
 export default notificationRoute;
