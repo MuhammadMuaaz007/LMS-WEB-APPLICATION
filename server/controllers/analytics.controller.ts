4058a0694398c346c5092d85cb86b33374bb4246
Muaaz <mrmuaaz31@gmail.com>
2026-06-02 15:23:41 +0500

    Add analytics controller to retrieve user, order, and course data

---
+++ server/controllers/analytics.controller.ts
@@ -0,0 +1,49 @@
+import { Request, Response, NextFunction } from "express";
+import ErrorHandler from "../utils/ErrorHandler.js";
+import { CatchAsyncError } from "../middleware/catchAsyncErrors.js";
+import userModel from "../models/user.model.js";
+import orderModel from "../models/order.model.js";
+import CourseModel from "../models/course.model.js";
+import { generateLast12MonthData } from "../utils/analytics.generator.js";
+
+export const getUserAnalytics = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const users = await generateLast12MonthData(userModel);
+      res.status(200).json({
+        success: true,
+        users,
+      });
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 500));
+    }
+  },
+);
+
+export const getOrderAnalytics = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const orders = await generateLast12MonthData(orderModel);
+      res.status(200).json({
+        success: true,
+        orders,
+      });
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 500));
+    }
+  },
+);
+
+export const getCourseAnalytics = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const courses = await generateLast12MonthData(CourseModel);
+      res.status(200).json({
+        success: true,
+        courses,
+      });
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 500));
+    }
+  },
+);
