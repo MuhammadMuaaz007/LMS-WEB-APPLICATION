a9c8af44de1bef028dd3d97b66500e487ee22867
Muaaz <mrmuaaz31@gmail.com>
2026-06-08 16:14:05 +0500

    feat(client): enhance styling and layout with animations and theme provider

---
+++ client/app/page.tsx
@@ -0,0 +1,18 @@
+'use client'
+import React,{FC} from 'react';
+// import Heading from './utils/Heading';
+
+interface Props{
+  name:string,
+}
+
+const Page:FC<Props>=(props)=>{
+  return(
+    <div>
+      {/* <Heading title="SKILLSTACK" description="SKILLSTACK is a platform for learning and practicing programming skills" keywords="React,Nextjs,Programming,Redux,Machine Learning,Nodejs,MERN" /> */}
+      
+    </div>
+  )
+}
+
+export default Page
